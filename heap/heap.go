603493013c011b720c87c0501ae2cdc4fee1998e
JoseHuertaWebBeds <jose.m.huerta@gmail.com>
2020-10-14 22:55:25 +0200

    Heap: Refactor to hide the data array

--- heap/heap.go
+++ heap/heap.go
@@ -3,15 +3,17 @@ package heap
 // Heap represent a sorted heap to implement a priority queue
 // when pushin it will add an element to the queue, and sort it.
 // when poping it will get the highest element.
-type Heap []int
+type Heap struct {
+	data []int
+}
 
 // Push adds an element to the heap
 func (h *Heap) Push(i int) error {
-	*h = append(*h, i)
+	h.data = append(h.data, i)
 	return nil
 }
 
 // Pop gets and removes the highest element in the heap
 func (h *Heap) Pop() (int, error) {
-	return (*h)[0], nil
+	return h.data[0], nil
 }
