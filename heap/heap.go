bd2eb604a300958bcf2e1acbe3a94e099c7e5088
JoseHuertaWebBeds <jose.m.huerta@gmail.com>
2020-10-14 23:50:46 +0200

    Heap: Any structure, no errors returned

--- heap/heap.go
+++ heap/heap.go
@@ -10,10 +10,8 @@ type Heap struct {
 // Push adds an element to the heap
 func (h *Heap) Push(i int) error {
 	h.data = append(h.data, i)
-	n := h.last()
-	if h.data[n] > h.data[parent(n)] {
-		h.swap(n, parent(n))
-	}
+	h.orderUpFromLast()
+
 	return nil
 }
 
@@ -21,11 +19,68 @@ func (h *Heap) Push(i int) error {
 func (h *Heap) Pop() (int, error) {
 	out := h.data[0]
 	h.swap(0, h.last())
+	h.data = h.data[:h.last()]
+	h.orderDown(0)
 	return out, nil
 }
 
 func (h *Heap) last() int {
 	return len(h.data) - 1
+}
+
+func (h *Heap) orderUpFromLast() {
+	h.orderUp(h.last())
+}
+func (h *Heap) orderUp(i int) {
+	a := parent(i)
+	b := i
+	for h.order(a, b) {
+		if a == 0 {
+			break
+		}
+		b = a
+		a = parent(a)
+	}
+}
+
+func (h *Heap) order(a, b int) bool {
+	if h.data[a] < h.data[b] {
+		h.swap(a, b)
+		return true
+	}
+	return false
+}
+
+func (h *Heap) orderDown(i int) {
+	change := true
+
+	for change {
+		i, change = h.orderThree(i)
+	}
+}
+
+func (h *Heap) orderThree(a int) (int, bool) {
+	l := h.last()
+	b := leftChild(a)
+	c := rightChild(a)
+
+	max := a
+	if b <= l {
+		if h.data[b] > h.data[max] {
+			max = b
+		}
+	}
+	if c <= l {
+		if h.data[c] > h.data[max] {
+			max = c
+		}
+	}
+	if max > a {
+		h.swap(a, max)
+		return max, true
+	}
+	return a, false
+
 }
 
 func (h *Heap) swap(a, b int) {
@@ -35,8 +90,14 @@ func (h *Heap) swap(a, b int) {
 }
 
 func parent(i int) int {
-	if i == 0 {
-		return 0
-	}
 	return (i - 1) / 2
+}
+
+func leftChild(i int) int {
+	return i*2 + 1
+}
+
+func rightChild(i int) int {
+	return i*2 + 2
+
 }
--- heap/heap_test.go
+++ heap/heap_test.go
@@ -19,6 +19,14 @@ func TestPushAndPop(t *testing.T) {
 			[]int{1, 2},
 			[]int{2, 1},
 		},
+		{
+			[]int{1, 2, 3},
+			[]int{3, 2, 1},
+		},
+		{
+			[]int{1, 2, 3, 7, 0, -1, 6},
+			[]int{7, 6, 3, 2, 1, 0, -1},
+		},
 	}
 	for _, v := range vs {
 		h := new(Heap)
@@ -34,7 +42,7 @@ func TestPushAndPop(t *testing.T) {
 				t.Errorf("Unexpected error when pop %v\n", v.push)
 			}
 			if actualPop != expectedPop {
-				t.Errorf("%dth value.\nExpected Pop: %d\nActual Pop: %d\n", i+1, expectedPop, actualPop)
+				t.Errorf("%dth value of %v.\nExpected Pop: %d\nActual Pop: %d\n", i+1, v.push, expectedPop, actualPop)
 			}
 		}
 	}
